2146333cf22c83e35b46409b416a7c97b5248ffa
vikastiwari7744 <48121284+vikastiwari7744@users.noreply.github.com>
2019-06-22 17:56:31 +0530

    wrong code
    wrong here

--- Python/Basic_Data_Types/List_Comprehensions.py
+++ Python/Basic_Data_Types/List_Comprehensions.py
@@ -26,5 +26,4 @@ Y = int(raw_input())
 Z = int(raw_input())
 N = int(raw_input())
 
-ans = [[i, j, k] for i in range(X + 1) for j in range(Y + 1) for k in range(Z + 1) if i + j + k != N]
-print ans
+print( [[i,j,k] for i in range( x + 1) for j in range( y + 1) for k in range(z+1) if ( ( i + j + k ) != n )  ])
